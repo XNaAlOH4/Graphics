07bfc74cbaf06616cb525b29a5830c324a9a3052
XNaAlOH4 <74347686+XNaAlOH4@users.noreply.github.com>
2020-12-11 21:37:33 +0800

    Create Snake.java

---
+++ Side Project/Snake.java
@@ -0,0 +1,15 @@
+public class Snake {
+  public Vector pos;
+  public Vector[] body;
+  public Snake(int x_, int y_) {
+    x = x_;
+    y = y_;
+  }
+  public void show() {
+    
+  }
+}
+
+public class Vector {
+  public int x,y;
+}
