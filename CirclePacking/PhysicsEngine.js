ce26cc3bb4cdac8d55d4a39d8d817598d60aa988
XNaAlOH4 <74347686+XNaAlOH4@users.noreply.github.com>
2020-12-11 17:14:35 +0800

    Update PhysicsEngine.js

--- CirclePacking/PhysicsEngine.js
+++ CirclePacking/PhysicsEngine.js
@@ -387,14 +387,8 @@ class Vector {
   stringify(last) {
   	switch(this.z) {
   	  case 0:
-	    if(!last) {
-		   return "{\"x\":"+this.x+','+"\"y\":"+this.y+"},";
-	    }
   	    return "{\"x\":"+this.x+','+"\"y\":"+this.y+"}";
   	  default:
-	    if(!last) {
-		   return "{\"x\":"+this.x+','+"\"y\":"+this.y+"\"z\":"+this.z+"},";
-	    }
   	    return "{\"x\":"+this.x+','+"\"y\":"+this.y+"\"z\":"+this.z+"}";
   	}
   }
